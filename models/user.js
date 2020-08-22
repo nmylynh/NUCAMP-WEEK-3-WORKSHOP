ebd0469eda78f073be066e779ff88e79be00132b
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2020-08-22 02:32:31 -0700

    chore: init

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
